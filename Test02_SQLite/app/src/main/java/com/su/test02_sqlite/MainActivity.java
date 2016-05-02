417fc3293261fab509c1c9e2107e0f42a5b9a037
TKUsu <202410089@s02.tku.edu.tw>
2016-05-02 17:36:21 +0800

    add Test02

---
+++ Test02_SQLite/app/src/main/java/com/su/test02_sqlite/MainActivity.java
@@ -0,0 +1,53 @@
+package com.su.test02_sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDBHelper dbHelper;
+    EditText tid, name, score, newScore, sql;
+    TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tid = (EditText)findViewById(R.id.editText);
+        name = (EditText)findViewById(R.id.editText2);
+        score = (EditText)findViewById(R.id.editText3);
+
+        output = (TextView)findViewById(R.id.textView6);
+
+        dbHelper = new MyDBHelper(this);
+        dbHelper.setValue(dbHelper.getWritableDatabase(), tid, name, score, output);
+
+    }
+
+    public void OnClick(View view){
+
+        switch (view.getId()){
+            case R.id.button:
+                Toast.makeText(MainActivity.this, "this is insert", Toast.LENGTH_LONG).show();
+                dbHelper.checkout("insert");
+                break;
+            case R.id.button2:
+                Toast.makeText(MainActivity.this,"this is search",Toast.LENGTH_LONG).show();
+                dbHelper.checkout("search");
+                break;
+            case R.id.button3:
+                Toast.makeText(MainActivity.this,"this is update",Toast.LENGTH_LONG).show();
+                dbHelper.checkout("update");
+                break;
+            case R.id.button4:
+                Toast.makeText(MainActivity.this,"this is delete",Toast.LENGTH_LONG).show();
+                dbHelper.checkout("delete");
+                break;
+        }
+    }
+}
---
+++ Test02_SQLite/app/src/main/java/com/su/test02_sqlite/MyDBHelper.java
@@ -0,0 +1,103 @@
+package com.su.test02_sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by ibelieveican on 2015/7/7.
+ */
+public class MyDBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "todo";
+    private static final int DATABASE_VERSION = 1;
+    EditText tid, name, score, newScore, sql;
+    TextView output;
+    private SQLiteDatabase db;
+
+    public MyDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    public void setValue(SQLiteDatabase db, EditText tid, EditText name, EditText score, TextView output) {
+        this.db = db;
+        this.tid = tid;
+        this.name = name;
+        this.score = score;
+        this.output = output;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE road (" +
+                " _id INTEGER PRIMARY KEY," +
+                " name TEXT," +
+                " score REAL)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+    }
+
+
+
+
+
+        public void checkout(String temp) {
+            ContentValues cv;
+            int count, id;
+            switch (temp) {
+                case "insert":
+                    long idtemp;
+                    cv = new ContentValues();
+                    cv.put("_id", Integer.parseInt(tid.getText().toString()));
+                    cv.put("name", name.getText().toString());
+                    cv.put("score", Double.parseDouble(score.getText().toString()));
+                    idtemp = db.insert("road", null, cv);
+                    output.setText("新增記錄成功" + idtemp);
+                    break;
+                case "search":
+                    SqlQuery("SELECT * FROM " + "road");
+                    break;
+                case "update":
+                    id = Integer.parseInt(tid.getText().toString());
+                    cv = new ContentValues();
+                    cv.put("score", Double.parseDouble(score.getText().toString()));
+                    count = db.update("road", cv, "_id=" + id, null);
+                    output.setText("更新記錄成功" + count);
+                    break;
+                case "delete":
+                    id = Integer.parseInt(tid.getText().toString());
+                    count = db.delete("road" +
+                            "", "_id=" + id, null);
+                    output.setText("刪除記錄成功" + count);
+                    break;
+            }
+        }
+
+        public void SqlQuery(String sql) {
+            String[] colNames;
+            String str = "";
+            Cursor c = db.rawQuery(sql, null);
+            colNames = c.getColumnNames();
+            for (int i = 0; i < colNames.length; i++) {
+                str += colNames[i] + "\t\t";
+            }
+            str += "\n";
+            c.moveToFirst();
+            for (int i = 0; i < c.getCount(); i++) {
+                str += c.getString(0) + "\t";
+                str += c.getString(1) + "\t";
+                str += c.getString(2) + "\n";
+                c.moveToNext();
+            }
+            output.setText(str.toString());
+        }
+}
